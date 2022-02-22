f4db8b8ea5414ef85e0ed7b0601103a1f9989595
cindy0817-web <s2103@e-mirim.hs.kr>
2022-02-22 15:22:27 +0900

    var(변수 선언), typeof(변수의 크기)

---
+++ lecture04.js
@@ -0,0 +1,11 @@
+var a=100;
+console.log(a,typeof(a));
+//100 "number"라고 출력
+
+var c="안녕하세요";
+console.log(c,typeof(c));
+//안녕하세요 "string"라고 출력
+
+var e=true;
+console.log(e,typeof(e));
+//true "boolean"라고 출력
