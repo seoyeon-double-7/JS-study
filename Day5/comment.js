575ebc820ee310f6dd39b03ab75b582b6f14ff79
cindy0817-web <s2103@e-mirim.hs.kr>
2022-02-27 15:55:22 +0900

    JS 주석 // /* */

---
+++ Day5/comment.js
@@ -0,0 +1,12 @@
+//프로그램에 대한 설명, 라이센스에 대한 명시
+//작성자에 대한 정보
+var name = prompt("이름을 입력해 주세요");
+
+var msg = "당신의 이름은" + name + " 입니다." +
+        name + "님의 이름은" + name.lnegth + "글자 입니다."
+
+// console.log(msg) 콘솔에서 msg 출력
+alert(msg);
+/*
+여러줄 주석입니다.
+ */
