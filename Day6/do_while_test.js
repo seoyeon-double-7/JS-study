e77174eb91a8956f1f2d915ba42b4906f944fa1b
cindy0817-web <s2103@e-mirim.hs.kr>
2022-02-28 15:42:03 +0900

    for문 반복문

--- Day6/do_while_test.js
+++ Day6/do_while_test.js
@@ -6,3 +6,9 @@ while(cond){
 do{
     console.log("이 구문 한번 실행됨")
 }while (cond);
+
+do{
+     var ans = parseInt(prompt("1+1+2"));
+}while(ans != 4)
+
+console.log("맞췄습니다.")
---
+++ Day6/for_test 01.js
@@ -0,0 +1,16 @@
+var arrays = [1,2,3,4,5,6,7,8,9,10]
+
+var i = 0;
+while(i = arrays.length){
+
+    console.log(arrays[i])
+    i++;
+}
+
+for(var i = 0; i<arrays.length; i++){
+    console.log(arrays[i])
+}
+//
+// for (var array in arrays) {
+//     console.log(array)
+// }
